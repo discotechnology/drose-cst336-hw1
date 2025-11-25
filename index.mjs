9fa12c3cbb72b5d3fb72aee71774c1aa79c40f68
discotechnology <drosenfeld@csumb.edu>
2025-11-24 16:49:40 -0800

    Rebuilt other pages, updated index.mjs

--- index.mjs
+++ index.mjs
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
    res.render('index');
 });
 
+app.get('/history', (req, res) => {
+   res.render('history');
+});
+
+app.get('/media', (req, res) => {
+   res.render('media');
+});
+
+app.get('/protocols', (req, res) => {
+   res.render('protocols');
+});
+
 app.listen(3000, () => {
    console.log('server started');
 });
